8110ccdb5f61d08e62d67e854dae0fb8b1b66f87
MADNESSZ1RA <92206761+MADNESSZ1RA@users.noreply.github.com>
2023-01-15 17:11:06 +0300

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -1,4 +1,7 @@
 import os
+import tkinter
+import tkinter.filedialog as fd
+from requests import get
 from tkinter import *
 
 window = Tk()
@@ -6,9 +9,11 @@ window.title("MADNESS sorter")
 window.geometry('200x150')
 window['bg'] = 'blue'
 
-
 def com():
-    path = edit.get()
+    path = fd.askdirectory(title="Открыть папку", initialdir="/")
+    if path:
+        print(path)
+
     files = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
 
     extention = list(set([file.split('.')[-1] for file in files]))
@@ -26,32 +31,7 @@ def com():
                 pass
 
 
-def rabbit():
-    pas = abc.get()
-    try:
-        if pas == "хуй":
-            lbl.configure(text="Люблю тебя")
-        else:
-            pass
-    except:
-        pass
+qwer = tkinter.Button(window, text='Выбрать файл', command=com)
+qwer.pack()
 
-
-a = Label(window, font=15, width=25, text='Введите путь', fg='black', borderwidth=0, background='blue')
-a.pack()
-
-edit = Entry(window, width=30)
-edit.pack()
-
-abc = Entry(window, width=30, background='blue', borderwidth=0)
-abc.pack()
-
-but = Button(window, text='Ввод', command=com)
-but.pack()
-
-but2 = Button(window, text='      ', command=rabbit, background='blue', borderwidth=0)
-but2.pack()
-
-lbl = Label(window, text="", font=("Arial Bold", 20), background='blue', borderwidth=0)
-lbl.pack()
 window.mainloop()
