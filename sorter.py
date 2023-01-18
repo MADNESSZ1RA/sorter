dc7ce63aa3e811ea21dad0734be6936ed3436008
MADNESSZ1RA <92206761+MADNESSZ1RA@users.noreply.github.com>
2023-01-19 00:34:11 +0300

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -9,34 +9,38 @@ window.title("MADNESS sorter")
 window.geometry('200x150')
 window['bg'] = 'blue'
 
-exclusion = ""
-
 
 def choose_files():
+    global files, path, extentions
     path = fd.askdirectory(title="Открыть папку", initialdir="/")
     files = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
-    extention = list(set([file.split('.')[-1] for file in files]))
-    for exclusion in extention:
-        exclusion = tkinter.Checkbutton(window, text=exclusion, variable=exclusion)
+    extentions = list(set([file.split('.')[-1] for file in files]))
+    for exclusion in extentions:
+        exclusion = tkinter.Checkbutton(window, text=exclusion, command=kiejgnbwek)
         exclusion.pack()
-        print(exclusion)
 
-    if
 
+def kiejgnbwek():
+    for abc in extentions:
         try:
-            os.mkdir(os.path.join(path, b))
+            os.mkdir(os.path.join(path, abc))
         except:
             pass
         for bca in files:
+            print(bca)
+            print(abc)
+            print(path)
             try:
-                if bca.endswith(b):
-                    os.replace(os.path.join(path, bca), os.path.join(path, b, bca))
+                if bca.endswith(abc):
+                    os.replace(os.path.join(path, bca, abc), os.path.join(path, abc, bca))
             except:
                 pass
 
-
 qwer = tkinter.Button(window, text='Выбрать файл', command=choose_files)
 qwer.pack()
+
+# sorter = tkinter.Button(window, text='Рассортировать', command=)
+# sorter.pack()
 
 if __name__ == "__main__":
     window.mainloop()
