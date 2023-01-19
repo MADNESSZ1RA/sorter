2f65adbdf971b76a414543af73518f071ef52aa0
MADNESSZ1RA <92206761+MADNESSZ1RA@users.noreply.github.com>
2023-01-19 17:36:23 +0300

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -11,35 +11,25 @@ window['bg'] = 'blue'
 
 
 def choose_files():
-    global check, extentions, exclusion, files, path
+    global check, extentions, exclusion, files, path, exclusion
     path = fd.askdirectory(title="Открыть папку", initialdir="/")
     files = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
     extentions = list(set([file.split('.')[-1] for file in files]))
-    check = IntVar()
+    exclusion = IntVar()
     for exclusion in extentions:
-        exclusion = tkinter.Checkbutton(window, text=exclusion, command=kiejgnbwek, variable=check)
-        exclusion.pack()
+        exclusion = tkinter.Checkbutton(window, text=exclusion, textvariable=exclusion, command=command).pack()
         print(exclusion)
         print(extentions)
-        print(check)
 
 
-def kiejgnbwek():
+def command():
     for abc in extentions:
-        if check.get() == 1:
-            for abc in extentions:
-                try:
-                    os.mkdir(os.path.join(path, abc))
-                except:
-                    pass
-                for bca in files:
-                    try:
-                        if bca.endswith(abc):
-                            os.replace(os.path.join(path, bca), os.path.join(path, abc, bca))
-                    except:
-                        pass
+        if exclusion.get() == 0:
+            print('etylhknteythlketrryjnh')
+            print(abc)
+            print(exclusion.get)
         else:
-            print('ne robit')
+            print('tlkhjnewrtylkhjretwnyhlkerwtnhlkertyjhnletkyjhnk')
 
 
 qwer = tkinter.Button(window, text='Выбрать файл', command=choose_files)
