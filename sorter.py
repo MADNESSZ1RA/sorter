0cb37f604a8795437bab31d097d29b73a981e429
MADNESSZ1RA <92206761+MADNESSZ1RA@users.noreply.github.com>
2023-01-20 01:48:02 +0300

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -6,37 +6,61 @@ from tkinter import *
 
 window = Tk()
 window.title("MADNESS sorter")
-window.geometry('200x150')
-window['bg'] = 'blue'
+window.geometry('200x225')
+window['bg'] = 'black'
+window.iconbitmap('E:\фото\l9Ttgvh43bQ (1).ico')
 
 
 def choose_files():
-    global check, extentions, exclusion, files, path, exclusion
+    global files, path
     path = fd.askdirectory(title="Открыть папку", initialdir="/")
     files = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
     extentions = list(set([file.split('.')[-1] for file in files]))
-    exclusion = IntVar()
+    entry.insert(20, path)
+    entry.config(background='white', width=30)
+    intvar_dict.clear()
+
+    for cb in checkbutton_list:
+        cb.destroy()
+    checkbutton_list.clear()
+
     for exclusion in extentions:
-        exclusion = tkinter.Checkbutton(window, text=exclusion, textvariable=exclusion, command=command).pack()
-        print(exclusion)
-        print(extentions)
+        intvar_dict[exclusion] = tkinter.IntVar()
+        c = tkinter.Checkbutton(window, text=exclusion, variable=intvar_dict[exclusion])
+        c.pack()
+        checkbutton_list.append(c)
 
 
-def command():
-    for abc in extentions:
-        if exclusion.get() == 0:
-            print('etylhknteythlketrryjnh')
-            print(abc)
-            print(exclusion.get)
+def sort():
+    for key, value in intvar_dict.items():
+        if value.get() > 0:
+            try:
+                for hold in key:
+                    os.mkdir(os.path.join(path, key))
+                    for bca in files:
+                        try:
+                            if bca.endswith(key):
+                                os.replace(os.path.join(path, bca), os.path.join(path, key, bca))
+                        except:
+                            pass
+            except:
+                pass
+
         else:
-            print('tlkhjnewrtylkhjretwnyhlkerwtnhlkertyjhnletkyjhnk')
+            print("Не выбрано:", key)
 
 
-qwer = tkinter.Button(window, text='Выбрать файл', command=choose_files)
+intvar_dict = {}
+checkbutton_list = []
+
+qwer = tkinter.Button(window, text="Выбрать папку", command=choose_files)
 qwer.pack()
 
-# sorter = tkinter.Button(window, text='Рассортировать', command=)
-# sorter.pack()
+entry = tkinter.Entry(window, background='black', borderwidth=0)
+entry.pack()
 
-if __name__ == "__main__":
-    window.mainloop()
+btn1 = tkinter.Button(window, text="Рассортировать", command=sort)
+btn1.pack()
+
+window.mainloop()
+window = tkinter.Tk()
